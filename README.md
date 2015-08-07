5db98c4d0eb43302997edd745ce618e22287d959
Dave Webb <github@d5ve.com>
2015-08-08 09:16:21 +1200

    DCW - Initial commit for Net::PhotoBackup::Server
    $ sudo cpan Minilla
    $ minil new Net-PhotoBackup-Server
    $ mv Net-PhotoBackup-Server p5-Net-PhotoBackup-Server

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Net::PhotoBackup::Server - It's new $module
+
+# SYNOPSIS
+
+    use Net::PhotoBackup::Server;
+
+# DESCRIPTION
+
+Net::PhotoBackup::Server is ...
+
+# LICENSE
+
+Copyright (C) Dave Webb.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Dave Webb <github@d5ve.com>
---
+++ lib/Net/PhotoBackup/Server.pm
@@ -0,0 +1,39 @@
+package Net::PhotoBackup::Server;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::PhotoBackup::Server - It's new $module
+
+=head1 SYNOPSIS
+
+    use Net::PhotoBackup::Server;
+
+=head1 DESCRIPTION
+
+Net::PhotoBackup::Server is ...
+
+=head1 LICENSE
+
+Copyright (C) Dave Webb.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Dave Webb E<lt>github@d5ve.comE<gt>
+
+=cut
+
