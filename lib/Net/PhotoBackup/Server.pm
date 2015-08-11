bbeca40f9013713fe23db3723c59a9b4ba2da3cf
Dave Webb <wfh@dcw.co.nz>
2015-08-11 15:10:04 +1200

    Add run(), app(), and index_html()

--- lib/Net/PhotoBackup/Server.pm
+++ lib/Net/PhotoBackup/Server.pm
@@ -29,11 +29,14 @@ use warnings;
 
 =cut
 
-use File::HomeDir ();
-use Digest::SHA ();
 use Data::Dumper; $Data::Dumper::Sortkeys = 1;
+use Digest::SHA ();
+use File::HomeDir ();
+use Plack::Request;
+use Plack::Runner;
 
 our $VERSION = "0.01";
+sub version { $VERSION }
 
 =head2 new()
 
@@ -184,6 +187,136 @@ sub config {
         }
     }
     return $config;
+}
+
+=head2 run()
+
+Launch the PhotoBackup web service using config from the conf file.
+
+=cut
+
+sub run {
+    my $self = shift;
+
+    $self->init unless $self->config;
+
+    my $config = $self->config;
+
+    my @args = (
+        app        => $self->get_app,
+        daemonize  => 1
+        env        => 'deployment',
+        port       => $config->{Port},
+        server     => 'Starman',
+        version_cb => \&version,
+    );
+
+    my $runner = Plack::Runner->new(@args);
+  
+    $runner->run; 
+     
+}
+
+=head2 app()
+
+Return the PSGI application subref.
+
+=cut
+
+sub app {
+    my $self = shift;
+    my $config = shift || $self->config;
+
+    return sub {
+        my $env = shift; # PSGI env
+ 
+        my $req = Plack::Request->new($env);
+        my $path_info = $req->path_info;
+        my $method = $req->method;
+
+        if ( $path_info eq '' || $path_info eq '/' ) {
+            # GET / : Return a HTML doc describing PhotoBackup.
+
+            # POST / : Store new image file in MediaRoot. Needs password.
+        }
+        elsif ( $path_info eq '/test' ) {
+
+        }
+
+
+        # POST /test : Check password, then attempt to write test file to MediaRoot.
+    };
+}
+
+=head2 index_html()
+
+Returns the info page about PhotoBackup.
+
+Copied from: https://raw.githubusercontent.com/PhotoBackup/server-flask/master/templates/index.html
+@f47b8ffb4c3aa223071e9f0acde46f7b1cd62ef9
+
+=cut
+
+sub index_html {
+q{
+<!DOCTYPE html>
+<!--
+    Copyright (C) 2013-2015 Stéphane Péchard.
+
+    This file is part of PhotoBackup.
+
+    PhotoBackup is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PhotoBackup is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PhotoBackup. If not, see <http://www.gnu.org/licenses/>.
+-->
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="PhotoBackup is used to backup
+    your pictures from a mobile device to your own server. OWN YOUR DATA!">
+
+    <title>PhotoBackup server</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+</head>
+<body>
+
+    <div class="container">
+        <div style="text-align:center;">
+            <h1>PhotoBackup</h1>
+            <p class="lead">Backup your pictures from your mobile device to your server.<br />OWN YOUR DATA!</p>
+        </div>
+
+        <div class="jumbotron">
+            <p>PhotoBackup works with two applications, a client and a server,
+                dedicated to the backup of the pictures you take on your
+                mobile device to your own server.</p>
+            <p>You need four little steps:</p>
+
+            <ol style="font-size:1.4em;">
+                <li><a href="http://play.google.com">Install a mobile application</a> ;</li>
+                <li><a href="https://github.com/stephanepechard/photobackup-servers">
+                    Install one of the server applications</a> ;
+                </li>
+                <li>Take pictures as usual and let the magic happens in background ;</li>
+                <li>Keep your pictures safe on your server.</li>
+            </ol>
+        </div>
+    </div>
+
+</body>
+</html>
+};
 }
 
 1;
